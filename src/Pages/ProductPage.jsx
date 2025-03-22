ddf8a66037212eeeaf551e68f39af306f354d119
Yash Bhamre <89498904+Yash0259@users.noreply.github.com>
2025-03-22 14:43:46 +0530

    Update ProductPage.jsx

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -15,7 +15,7 @@ export const ProductPage = () => {
     // Fetch products from API
     const fetchProducts = async () => {
         try {
-            const response = await fetch(`${API_URL}/products`);
+            const response = await fetch(`${API_URL}`);
             if (!response.ok) throw new Error("Failed to fetch products");
             const data = await response.json();
             setProducts(data);
@@ -56,7 +56,7 @@ export const ProductPage = () => {
             }
         });
 
-        const response = await fetch(`${API_URL}/products`, {
+        const response = await fetch(`${API_URL}`, {
             method: "POST",
             body: formDataToSend,
         });
